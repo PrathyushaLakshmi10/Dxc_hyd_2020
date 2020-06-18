cad45bf57e68a38ff6873171607003cad8452d73
patti <patti@DESKTOP-1VQDN1U>
2020-06-18 15:06:30 +0530

    banking appln

---
+++ Dxc first/src/com/dxc/bankingapppoc/HDFC.java
@@ -0,0 +1,33 @@
+package com.dxc.bankingapppoc;
+import java.io.*;
+public class HDFC implements RBI {
+
+	
+	  public   long balance;
+      public int MIN_BALANCE=2500;
+@Override
+public void openAccount(BufferedReader buff) {
+	// TODO Auto-generated method stub
+
+}
+
+@Override
+public void deposit(BufferedReader buff) {
+	// TODO Auto-generated method stub
+
+}
+
+@Override
+public void withdrawl(BufferedReader buff) {
+	// TODO Auto-generated method stub
+}
+@Override
+public void openFD(BufferedReader buff) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+
+
+}
