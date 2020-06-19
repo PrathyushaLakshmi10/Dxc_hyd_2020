34a0fcd7e728ccb513193db641fbec4faaa6e920
patti <patti@DESKTOP-1VQDN1U>
2020-06-19 11:37:22 +0530

    after the changes made

--- Dxc first/src/com/dxc/bankingapppoc/CustomerBanking.java
+++ Dxc first/src/com/dxc/bankingapppoc/CustomerBanking.java
@@ -1,16 +1,21 @@
 package com.dxc.bankingapppoc;
-import java.io.*;
-class MyBankClass <T> {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+// This is My Customer Banking Class
+class MyBankClass <T> { // T -> ICICI / HDFC / AXIS
 	T obj;
 	
-	void create(T obj) {
+	void create(T obj) { // T-> ICICI / HDFC / AXIS
 		this.obj = obj;
 	}
 	
-	T getInstance() {
+	T getInstance() { // T -> ICICI / HDFC / AXIS
 		return obj;
 	}
 }
+
 public class CustomerBanking {
 
 	InputStreamReader isr = null;
@@ -18,42 +23,202 @@ public class CustomerBanking {
 	ICICI mICICI = null;
 	HDFC mHDFC = null;
 	AXIS mAxis = null;
-	
+
 	public static void main(String[] args) throws Exception {
 		
 		CustomerBanking mObject = new CustomerBanking();
 		
 		mObject.isr = new InputStreamReader(System.in);
 		mObject.buff = new BufferedReader(mObject.isr);
+		int axiscustomer=0;
+		int icicicustomer=0;
+		int hdfccustomer=0;
+		int no_ofopenacc=0;
+		int no_ofdeposit=0;
+		int no_ofwithdrawls=0;
+		int no_ofopenFd=0;
+		String mOperationChoice, continueChoice;
+		boolean transaction = true;
+		String continuebankchoice;
+		boolean bankaccess=true;
+		do{
+			System.out.println("Welcome to INDIAN BANKING SYSTEM\n");
+			System.out.println("SELECT  YOUR BANK\n1. ICICI\n2. HDFC\n3. AXIS\n4.None of these");
+			String mBankChoice = mObject.buff.readLine();
+			
+			if(Integer.parseInt(mBankChoice)==1)
+			{
+				icicicustomer=icicicustomer+1;
+				System.out.println("count of customers visited:"+icicicustomer);
+			}
+			else if(Integer.parseInt(mBankChoice)==2)
+			{
+				hdfccustomer=hdfccustomer+1;
+				System.out.println("count of customers visited:"+hdfccustomer);
+			}
+			else if(Integer.parseInt(mBankChoice)==3)
+			{
+				axiscustomer=axiscustomer+1;
+				System.out.println("count of customers visited:"+axiscustomer);
+			}
 		
-		System.out.println("Welcome to INDIAN BANKING SYSTEM\n");
-		System.out.println("SELECT  YOUR BANK\n1. ICICI\n2. HDFC\n3. AXIS");
-		String mBankChoice = mObject.buff.readLine();
+			
+			switch(Integer.parseInt(mBankChoice))
+			{
+				case 1: MyBankClass<ICICI> obj = new MyBankClass<ICICI>();
+						obj.create(new ICICI());
+						mObject.mICICI = obj.getInstance();
+			
+						do{
+							System.out.println("SELECT  YOUR  Operation\n1. Open Account\n2. Deposit\n3. WithDrawl\n4. Open FD");
+							mOperationChoice = mObject.buff.readLine();
+							if(Integer.parseInt(mOperationChoice)==1)
+							{
+								no_ofopenacc=no_ofopenacc+1;
+								System.out.println("no.of times account opened "+no_ofopenacc);
+							}
+							else if(Integer.parseInt(mOperationChoice)==2)
+							{
+								no_ofdeposit=no_ofdeposit+1;
+								System.out.println("no.of times amount deposited "+no_ofdeposit);
+							}
+								
+							
+							else if(Integer.parseInt(mOperationChoice)==3)
+							{
+								no_ofwithdrawls=no_ofwithdrawls+1;
+								System.out.println("no.of times amount withdraw "+no_ofwithdrawls);
+							}
+							else if(Integer.parseInt(mOperationChoice)==4)
+							{
+								no_ofopenFd=no_ofopenFd+1;
+								System.out.println("no.of times open FD "+no_ofopenFd);
+							}
+							switch(Integer.parseInt(mOperationChoice)) 
+							{
+								case 1: mObject.mICICI.openAccount(mObject.buff);
+									break;
+								case 2: mObject.mICICI.deposit(mObject.buff);
+									break;
+								case 3: mObject.mICICI.withdrawl(mObject.buff);
+									break;
+								case 4: mObject.mICICI.openFD(mObject.buff);
+									break;
+								default: System.out.println("No Operation Choice Selected");
+							}
+					
+							System.out.println("Do you Want to Continue\nYes\nNo");
+							continueChoice = mObject.buff.readLine();
+							if(continueChoice.equalsIgnoreCase("No"))
+								transaction = false;
+						  }while(transaction);
+						  break;
+					
+				
+			
+				case 2: MyBankClass<HDFC> obj1 = new MyBankClass<HDFC>();
+						obj1.create(new HDFC());
+						mObject.mHDFC = obj1.getInstance();
+						do
+						{
+							System.out.println("SELECT  YOUR  Operation\n1. Open Account\n2. Deposit\n3. WithDrawl\n4. Open FD");
+							mOperationChoice = mObject.buff.readLine();
+							if(Integer.parseInt(mOperationChoice)==1)
+							{
+								no_ofopenacc=no_ofopenacc+1;
+								System.out.println("no.of times account opened "+no_ofopenacc);
+							}
+							else if(Integer.parseInt(mOperationChoice)==2)
+							{
+								no_ofdeposit=no_ofdeposit+1;
+								System.out.println("no.of times amount deposited "+no_ofdeposit);
+							}
+								
+							
+							else if(Integer.parseInt(mOperationChoice)==3)
+							{
+								no_ofwithdrawls=no_ofwithdrawls+1;
+								System.out.println("no.of times amount withdraw "+no_ofwithdrawls);
+							}
+							else if(Integer.parseInt(mOperationChoice)==4)
+							{
+								no_ofopenFd=no_ofopenFd+1;
+								System.out.println("no.of times open FD "+no_ofopenFd);
+							}
+							switch(Integer.parseInt(mOperationChoice)) 
+							{
+								case 1: mObject.mHDFC.openAccount(mObject.buff);
+										break;
+								case 2: mObject.mHDFC.deposit(mObject.buff);
+										break;
+								case 3: mObject.mHDFC.withdrawl(mObject.buff);
+										break;
+								case 4:  mObject.mHDFC.openFD(mObject.buff);
+										break;
+								default: System.out.println("No Operation Choice Selected");
+							}
+							System.out.println("Do you Want to Continue\nYes\nNo");
+							continueChoice = mObject.buff.readLine();
+							if(continueChoice.equalsIgnoreCase("No"))
+								transaction = false;
+						}while(transaction);
+						break;
+			
+				case 3: MyBankClass<AXIS> obj2 = new MyBankClass<AXIS>();
+						obj2.create(new AXIS());
+						mObject.mAxis = obj2.getInstance();
+						do
+						{
+							System.out.println("SELECT  YOUR  Operation\n1. Open Account\n2. Deposit\n3. WithDrawl\n4. Open FD");
+							mOperationChoice = mObject.buff.readLine();
+							if(Integer.parseInt(mOperationChoice)==1)
+							{
+								no_ofopenacc=no_ofopenacc+1;
+								System.out.println("no.of times account opened "+no_ofopenacc);
+							}
+							else if(Integer.parseInt(mOperationChoice)==2)
+							{
+								no_ofdeposit=no_ofdeposit+1;
+								System.out.println("no.of times amount deposited "+no_ofdeposit);
+							}
+								
+							
+							else if(Integer.parseInt(mOperationChoice)==3)
+							{
+								no_ofwithdrawls=no_ofwithdrawls+1;
+								System.out.println("no.of times amount withdraw "+no_ofwithdrawls);
+							}
+							else if(Integer.parseInt(mOperationChoice)==4)
+							{
+								no_ofopenFd=no_ofopenFd+1;
+								System.out.println("no.of times open FD "+no_ofopenFd);
+							}
+							switch(Integer.parseInt(mOperationChoice))
+							{
+								case 1: mObject.mAxis.openAccount(mObject.buff);
+										break;
+								case 2: mObject.mAxis.deposit(mObject.buff);
+										break;
+								case 3: mObject.mAxis.withdrawl(mObject.buff);
+										break;
+								case 4:  mObject.mAxis.openFD(mObject.buff);
+										break;
+								default: System.out.println("No Operation Choice Selected");
+							}
+							System.out.println("Do you Want to Continue\nYes\nNo");
+							continueChoice = mObject.buff.readLine();
+							if(continueChoice.equalsIgnoreCase("No"))
+								transaction = false;
+						}while(transaction);
+						break;
 		
-		switch(Integer.parseInt(mBankChoice))
-		{
-		case 1: MyBankClass<ICICI> obj = new MyBankClass<ICICI>();
-				obj.create(new ICICI());
-				mObject.mICICI = obj.getInstance();
-				break;
-		
-		case 2: MyBankClass<HDFC> obj1 = new MyBankClass<HDFC>();
-				obj1.create(new HDFC());
-				mObject.mHDFC = obj1.getInstance();
-				break;
-		
-		case 3: MyBankClass<AXIS> obj2 = new MyBankClass<AXIS>();
-				obj2.create(new AXIS());
-				mObject.mAxis = obj2.getInstance();
-				break;
-		
-		default: System.out.println("No Choice of Bank Done");
-		}
-
-
-
-
-
-	}	
+				default: System.out.println("No Choice of Bank Done");
+			}
+						 System.out.println("do you want ot continue\nYes\nNo");
+						 continuebankchoice=mObject.buff.readLine();
+						 if(continuebankchoice.equalsIgnoreCase("No"))
+							bankaccess=false;
+			
+		 }while(bankaccess);
+	}
 }
-
--- Dxc first/src/com/dxc/bankingapppoc/ICICI.java
+++ Dxc first/src/com/dxc/bankingapppoc/ICICI.java
@@ -1,30 +1,78 @@
 package com.dxc.bankingapppoc;
 import java.io.*;
 public  class ICICI implements RBI{
-	  public   long balance;
-      public int MIN_BALANCE=2000;
-@Override
-public void openAccount(BufferedReader buff) {
-	// TODO Auto-generated method stub
-
-}
-
-@Override
-public void deposit(BufferedReader buff) {
-	// TODO Auto-generated method stub
-
-}
-
-@Override
-public void withdrawl(BufferedReader buff) {
-	// TODO Auto-generated method stub
-}
-@Override
-public void openFD(BufferedReader buff) {
-	// TODO Auto-generated method stub
+	public int ROI = 5;
+	public int MIN_FD_AMOUNT = 5000;
+	public int MIN_YEARS = 2;
+	public int MIN_BAL = 3500;
+	public int BAL = MIN_BAL;
 	
-}
+	@Override
+	public void openAccount(BufferedReader buff) {
+		try {int count=0;
+		  
+			System.out.println("Enter your Name");
+			String name = buff.readLine();
+			System.out.println("Enter your Email");
+			String email = buff.readLine();
+			System.out.println("Enter your Phone");
+			String phone = buff.readLine();
+			
+			System.out.println("Congratulations your Account is Opened !!");
+			System.out.println("your Details are \nName: "+name+"\nEmail: "+email+"\nPhone: "+phone);
+		}
+		catch(Exception e) {
+			System.out.println("Exception is "+e);
+		}
+	}
+
+	@Override
+	public void deposit(BufferedReader buff) {
+		try {
+			System.out.println("Enter the amount to be deposited");
+			String amount = buff.readLine();
+			BAL = BAL + Integer.parseInt(amount);
+			System.out.println("Balance is "+BAL);
+		}
+		catch(Exception e) {
+			System.out.println("Exception is "+e);
+		}
+	}
+
+	@Override
+	public void withdrawl(BufferedReader buff) {
+		try {
+			System.out.println("Enter the amount to be Withdrawl");
+			String amount = buff.readLine();
+			if(BAL - Integer.parseInt(amount) > MIN_BAL)
+				BAL = BAL - Integer.parseInt(amount);
+			else
+				System.out.println("MIN BAL is not maintained after withdrawl !!");
+			System.out.println("Balance is "+BAL);
+		}
+		catch(Exception e) {
+			System.out.println("Exception is "+e);
+		}
+	}
+
+	@Override
+	public void openFD(BufferedReader buff) {
+		try {
+			System.out.println("Enter your FD amount");
+			String fdamount = buff.readLine();
+			System.out.println("Enter Term Duration of FD");
+			String years = buff.readLine();
+			if(Integer.parseInt(fdamount) > MIN_FD_AMOUNT && Integer.parseInt(years) > MIN_YEARS) {
+				int TotalFDAmount = Integer.parseInt(fdamount) + (ROI*Integer.parseInt(fdamount));
+				for(int i=2; i<=Integer.parseInt(years); i++)
+					TotalFDAmount = TotalFDAmount + (ROI*TotalFDAmount);
+				System.out.println("Your Total FD Amount after "+years+" years will be "+TotalFDAmount);
+			}
+				
+		}
+		catch(Exception e) {
+			System.out.println("Exception is "+e);
+		}
+	}
 
 }
-
-
